1864b40b95ff790ad2ca233ac3b0a3598777329b
AshFromReunion <ash-concept@hotmail.fr>
2016-06-06 08:26:19 +0400

    ajout cv

--- apprenant.php
+++ apprenant.php
@@ -227,7 +227,7 @@
                         </div>
                         <ul class="list-inline portfolio">
                             <li><a href="#" target="_blank" class="btn btn-default btn-md disabled">Portfolio</a></li>
-                            <li><a href="#" target="_blank" class="btn btn-default btn-md disabled">CV</a></li>
+                            <li><a href="cv/atchiaman-david.pdf" target="_blank" class="btn btn-default btn-md">CV</a></li>
                         </ul>
                     </div>
                     <a href="contact-apprenant.php" target="_blank" class="btn btn-default contacter" role="button">RECRUTER</a>
